19e43c268cf9e2d43b8647e321693703173c203b
tuguldurzolzaya <togzolzaya23@icloud.com>
2026-07-27 15:05:02 +0100

    Implement analytical Fourier transform and plotting functions
    Refactor analytical Fourier transform functions and add plotting capabilities.

--- fourierror/analytical.py
+++ fourierror/analytical.py
@@ -1,80 +1,254 @@
-# import numpy as np
-# import matplotlib.pyplot as plt
-# from mpl_toolkits.mplot3d import Axes3D
-# import os
+import numpy as np
+import scipp as sc
+import matplotlib.pyplot as plt
+from functions import *
+from scipy.stats import norm
+import scipy.stats as stats
 
-# filename = input("Enter the name of the file to load: ")
-# data = np.loadtxt(filename)
-# t = data[:, 0]
-# y_t = data[:, 1]
-# error_y = data[:, 2]
+# (1) Functions defined
 
-# # Constants used listed here:
-# N = t.size # number of time-domain data points
-# L = t[t.size-1] # length of time-domain timescale i.e. 0 <= t <= L (with N points)
-# Q = 5 # length of angular frequency domain i.e. 0 <= w <= Q (with N points)
-# w = np.linspace(0,Q,N) # angular frequency domain i.e. 0 <= w <= Q (with N points)
-# cov_y = np.diag(((error_y)*np.sqrt(N))**2)
-# # create a covariance matrix of the input error
-# # size = N x N
+def dft_anal(data: sc.DataArray, coord: str) -> sc.Dataset:
+    # "parameter: type" and "...type:" only to display expected parameter type for editor
+    # input requires "coord" but can be more specific i.e. {coord : "coord"}
 
-# dt = L / (N-1)
-# # step in t between consecutive data points between t(n) and t(n+1)
+    """
+    Performs an analytical Fourier transform outputting the real and imaginary parts separately
 
-# w_column = w[:, np.newaxis] # column vector of (w0, w1, w2... w(P-1))
-# # size = N
+    :param data: A scipp DataArray with the data to be Fourier transformed.
+    :param coord: The coordinate to compute the Fourier transform over.
 
-# time_domain = dt*np.arange(N) # (0dt, 1dt, 2dt... (N-1)dt)
-# # size = N
+    :returns: DataSet with the real, imaginary fourier transforms over the angular frequency coordinate
+    """
+    dft_matrix_manual = np.exp ( -2j * np.pi * ( np.arange(data.values.size)[:,np.newaxis] * np.arange(data.values.size)[:,np.newaxis].T ) / data.values.size)
+    # dft matrix of size = N x N
 
-# w_cross_t_matrix = w_column * time_domain
-# # size = N x N (rows: w, column: ndt)
+    f_w = (2 / data.values.size) * dft_matrix_manual @ data.values
+    # computes the matrix formulated fourier transform of form : f = A @ x
+    # size = N 
 
-# A = (2 / L) * np.exp( -1j * w_cross_t_matrix ) * dt
-# # gives the matrix A (c.f. f = Ax) with size = N x N
+    d = data.coords[coord][1:] - data.coords[coord][:-1]
+    # creates an array of length N-1 corresponding to the difference in x(n+1) and x(n)
+    if not sc.allclose(d, d[0]):
+        raise TypeError("Values must be evenly spaced to compute a DFT.")
+        # checks whether d[0] is equal to d for all d
 
-# f_w = A @ y_t
-# # gives the fourier transform at each w with size = N
+    cov = np.diag(data.variances)
 
-# cov_f = A @ cov_y @ A.conj().T
-# # cov_f = A*cov_y*A(T) but use @ to do matrix style multiplication instead of element-wise
+    cov_f = (2 / data.values.size)**2 * dft_matrix_manual @ cov @ dft_matrix_manual.conj().T
 
-# plt.plot(t,y_t, color="red")
-# plt.xlabel("Time / s")
-# plt.ylabel("y(t)")
-# plt.show()
-# # optional but shows the signal as a function of time
+    f_real = f_w.real
+    real = sc.array(
+        dims=["omega"], values=f_real, variances=np.diag(cov_f.real)
+    )
+    # "dims = ["omega"] means to attribute a coordinate that this data lies on here F(ω) lies on the ω-coordinate
+    # values are the mean values of the real part of the f_real matrix along "axis=1" which just means average across all the columns 
+    # covariances are computed between rows (most usefully between the same row : diagonal elements) and the ".diagonal()" takes the diagonal of the covariance matrix
 
-# plt.plot(w,abs(f_w), color="blue", label="F($\omega$)")
-# plt.ylabel("F($\omega$)")
-# plt.show()
-# # optional but shows the signal as a function of angular frequency
+    f_imag = f_w.imag
+    imag = sc.array(
+        dims=["omega"], values=f_imag, variances=np.diag(cov_f.imag)
+    )
+    # same as f_real but extracts the imaginary part
 
-# fig, ax = plt.subplots(figsize=(7, 6))
-# # draws a figure and a set of axes with figure size (7,6)
-# ax.set_xlabel("Data point, j")
-# ax.set_ylabel("Data point, i")
-# ax.set_title("Magnitude of covariance matrix")
-# im = ax.imshow(np.abs(cov_f), cmap='viridis', origin='upper',
-#                extent=[0, N-1, N-1, 0])
-# fig.colorbar(im, ax=ax, label='Covariance')
-# plt.show()
-# # optional but shows the covariance matrix as a heatmap
+    val = 1.0 / (data.values.size * d[0])
+    # value is just the conversion from k (index of f(k) to frequency of signal f(ν))
 
-# name_without_ext = os.path.splitext(filename)[0]
-# output_filename = f"{name_without_ext}_output_anal.txt"
-# np.savetxt(
-# f"{output_filename}.txt",
-# np.column_stack([w, abs(f_w), (np.sqrt(abs(np.diag(cov_f))))/np.sqrt(N)]),
-# header="omega\t|F(omega)|\terror_F",
-# fmt="%.8e",
-# delimiter="\t",
-# )
+    P = (data.values.size - 1) // 2 + 1
+    # take the size of data (N) and 
+        # if even : half it ( N/2 )
+        # if odd : half it ( N/2 ) and add 1 ( N/2 + 1 )
 
-# # Fix linting issues
-# ruff check . --fix
+    p1 = sc.arange(dim="omega", start=0, stop=P, step=1)
+    # create the freq-domain from 0 to (M-1)
+    # i.e. if N = 6 , P = 3 then p1 = [0,1,2]
 
-# # Commit the changes
-# git add .
-# git commit -m "Fix ruff linting and formatting issues"
-# git push
+    p2 = sc.arange(dim="omega", start=-(data.values.size // 2), stop=0, step=1)
+    # create freq-domain from 
+        # if even : -(N/2) to 0 
+        # if odd : [-(N/2) + 1] to 0
+
+    results = sc.concat([p1, p2], dim="omega")
+    # creates a zero-centered discrete freq-domain (if even then assymetry is pushed in negative direction)
+        # if N = 6 then results = [-3 , -2 , -1 , 0, 1 , 2 ]
+        # if N = 5 then results = [ -2 , -1 , 0 , 1 , 2 ]
+
+    ang_freq = results * val * 2 * np.pi
+    # multiply the index by 2π / (NΔt) to generate the angular frequency of the data
+
+    return sc.Dataset({"real": real, "imag": imag}, coords={"omega": ang_freq})
+
+dft_analdataset = dft_anal(data=data, coord="x")
+
+def dft_anal_mag(data: sc.DataArray, coord: str) -> sc.Dataset:
+    # "parameter: type" and "...type:" only to display expected parameter type for editor
+    # input requires "coord" but can be more specific i.e. {coord : "coord"}
+
+    """
+   Performs an analytical Fourier transform outputting the magnitude of a signal (signal can be anti/symmetrised)
+
+    :param data: A scipp DataArray with the data to be Fourier transformed.
+    :param coord: The coordinate to compute the Fourier transform over.
+
+    :returns: A scipp DataSet with the magnitude of the fourier transform and the angular frequency coordinate
+    """
+    dft_matrix_manual = np.exp ( -2j * np.pi * ( np.arange(data.values.size)[:,np.newaxis] * np.arange(data.values.size)[:,np.newaxis].T ) / data.values.size)
+    # dft matrix of size = N x N
+
+    f_w = (2 / data.values.size) * dft_matrix_manual @ data.values
+    # computes the matrix formulated fourier transform of form : f = A @ x
+    # size = N 
+
+    d = data.coords[coord][1:] - data.coords[coord][:-1]
+    # creates an array of length N-1 corresponding to the difference in x(n+1) and x(n)
+    if not sc.allclose(d, d[0]):
+        raise TypeError("Values must be evenly spaced to compute a DFT.")
+        # checks whether d[0] is equal to d for all d
+
+    cov = np.diag(data.variances)
+
+    cov_f = (2 / data.values.size)**2 * dft_matrix_manual @ cov @ dft_matrix_manual.conj().T
+
+    f_w = abs(f_w)
+    magnitude = sc.array(
+        dims=["omega"], values=f_w, variances=np.diag(abs(cov_f))
+    )
+
+
+    val = 1.0 / (data.values.size * d[0])
+    # value is just the conversion from k (index of f(k) to frequency of signal f(ν))
+
+    P = (data.values.size - 1) // 2 + 1
+    # take the size of data (N) and 
+        # if even : half it ( N/2 )
+        # if odd : half it ( N/2 ) and add 1 ( N/2 + 1 )
+
+    p1 = sc.arange(dim="omega", start=0, stop=P, step=1)
+    # create the freq-domain from 0 to (M-1)
+    # i.e. if N = 6 , P = 3 then p1 = [0,1,2]
+
+    p2 = sc.arange(dim="omega", start=-(data.values.size // 2), stop=0, step=1)
+    # create freq-domain from 
+        # if even : -(N/2) to 0 
+        # if odd : [-(N/2) + 1] to 0
+
+    results = sc.concat([p1, p2], dim="omega")
+    # creates a zero-centered discrete freq-domain (if even then assymetry is pushed in negative direction)
+        # if N = 6 then results = [-3 , -2 , -1 , 0, 1 , 2 ]
+        # if N = 5 then results = [ -2 , -1 , 0 , 1 , 2 ]
+
+    ang_freq = results * val * 2 * np.pi
+    # multiply the index by 2π / (NΔt) to generate the angular frequency of the data
+
+    return sc.Dataset({"magnitude": magnitude}, coords={"omega": ang_freq})
+
+dft_analdataset_mag = dft_anal_mag(data=data, coord="x")
+
+def plot_fourier(dft_dataset: sc.Dataset):
+    """
+    Plots the real/imaginary components (LHS) and the magnitude (RHS) of the fourier transform
+
+    Args:
+        dft_dataset : ScDataSet encoded with the {data: real(var), imag(var)} and {coords: "omega"}
+
+    Returns:
+        Two plots
+    """
+    omega = dft_dataset.coords["omega"].values
+    magnitude = ((dft_dataset["real"].values)**2 + (dft_dataset["imag"].values)**2)**0.5
+    real = dft_dataset["real"].values
+    imag = dft_dataset["imag"].values
+
+    fig, ax = plt.subplots(1,2,figsize=(12,4))  
+    ax[0].plot(omega, magnitude, color ="purple", label="Magnitude")
+    ax[0].set_xlabel(r"Angular frequency, $\omega$ / $\mathrm{rad}\,\mathrm{s}^{-1}$", fontsize=8)
+    ax[0].set_ylabel(r"Magnitude of Fourier Transform, |F($\omega$)|", fontsize=8)
+    ax[0].set_title(r"Magnitude of Fourier transform as a function of angular frequency", fontsize=8)
+    
+    ax[1].plot(omega, imag, color = "blue", label="Imaginary", alpha=0.5 )
+    ax[1].plot(omega , real,  color = "red", label="Real", alpha=0.5)
+    ax[1].set_xlabel(r"Angular frequency, $\omega$ / $\mathrm{rad}\,\mathrm{s}^{-1}$", fontsize=8)
+    ax[1].set_ylabel(r"Real/Imaginary component of Fourier Transform, F($\omega$)", fontsize=8)
+    ax[1].set_title(r"Real and Imaginary components of Fourier transform as a function of angular frequency", fontsize=8)
+    ax[1].legend()
+    plt.show()
+    return fig, ax
+
+def plot_fourier_mag(dft_dataset: sc.Dataset):
+    """
+    Plots the real/imaginary components (LHS) and the magnitude (RHS) of the fourier transform
+
+    Args:
+        dft_dataset : ScDataSet encoded with the {data: real(var), imag(var)} and {coords: "omega"}
+
+    Returns:
+        Two plots
+    """
+    omega = dft_dataset.coords["omega"].values
+    magnitude = (dft_dataset["magnitude"].values)
+
+    plt.plot(omega, magnitude)
+    plt.xlabel(r"Angular frequency, $\omega$ / $\mathrm{rad}\,\mathrm{s}^{-1}$", fontsize=8)
+    plt.ylabel(r"Magnitude of Fourier Transform, |F($\omega$)|", fontsize=8)
+    plt.title(r"Magnitude of Fourier transform as a function of angular frequency", fontsize=8)
+    plt.show()
+
+# (2) Data inputted and anti/symmetrised
+
+X = sc.linspace(dim='x', start=0, stop=2*np.pi, num=100, unit='rad')
+# creates a ScArray for time-domain
+
+Y = sc.sin(2 * X) + 2 * sc.cos(10 * X) + 0.6 * sc.sin(20 * X)
+# creates a ScArray corresponding to y(x) = sin(2x) + 2 cos(10x) + 0.6 sin(20x)
+
+Y_minus = sc.sin(-2 * X) + 2 * sc.cos(-10 * X) + 0.6 * sc.sin(-20 * X)
+
+Y_even = ( Y + Y_minus ) / 2
+Y_odd = ( Y - Y_minus ) / 2
+
+Y.variances = sc.abs(Y * 0.05)
+Y_even.variances = (np.sqrt(Y.variances) * np.sqrt(2) / 2)**2
+Y_odd.variances = (np.sqrt(Y.variances) * np.sqrt(2) / 2)**2
+
+Y_even.unit = sc.Unit('s')
+Y_odd.unit = sc.Unit('s')
+Y.unit = sc.Unit('s')
+# attributes units of "seconds" to y(x) 
+
+data = sc.DataArray(data=Y, coords={'x': X})
+# creates a DataArray for y(x) 
+# note (1) : coords parameter requires a dictionary as DataArray has atleast 0 coordinates that need to be specified
+
+data_symm = sc.Dataset(data = {"signal_even": Y_even, "signal_odd": Y_odd}, coords = {"x" : X})
+
+dft_numdataset = dft_num_uncomplex(data=data, coord="x", M=50000)
+
+dft_analdataset_mag_even = dft_anal_mag(data=data_symm["signal_even"], coord="x")
+plot_fourier_mag(dft_analdataset_mag_even)
+
+dft_analdataset_mag_odd = dft_anal_mag(data=data_symm["signal_odd"], coord="x")
+plot_fourier_mag(dft_analdataset_mag_odd)
+
+# (3) Plotting the comparison between the numerical and analytical error in the real and imaginary components
+
+omega = dft_analdataset_mag_even.coords["omega"].values
+error_num_real = np.sqrt(dft_numdataset["real"].variances)
+error_num_imag = np.sqrt(dft_numdataset["imag"].variances)
+error_anal_real = np.sqrt(dft_analdataset_mag_even["magnitude"].variances)
+error_anal_imag = np.sqrt(dft_analdataset_mag_odd["magnitude"].variances)
+
+fig, ax = plt.subplots(1,2,figsize=(12,4))  
+ax[0].plot(omega, error_num_real, color = "red", label="Numerical", alpha=0.5)
+ax[0].plot(omega , error_anal_real, color = "blue", label="Analytical", alpha=0.5)
+ax[0].set_xlabel(r"Angular frequency, $\omega$ / $\mathrm{rad}\,\mathrm{s}^{-1}$", fontsize=8)
+ax[0].set_ylabel(r"Error in real component of Fourier Transform", fontsize=8)
+ax[0].set_title(r"Error in real component compared between numerical and analytical", fontsize=8)
+ax[0].legend()
+
+ax[1].plot(omega, error_num_imag, color = "red", label="Numerical", alpha=0.5 )
+ax[1].plot(omega , error_anal_imag, color = "blue", label="Analytical", alpha=0.5)
+ax[1].set_xlabel(r"Angular frequency, $\omega$ / $\mathrm{rad}\,\mathrm{s}^{-1}$", fontsize=8)
+ax[1].set_ylabel(r"Error in imaginary component of Fourier Transform", fontsize=8)
+ax[1].set_title(r"Error in imaginary component compared between numerical and analytical", fontsize=8)
+ax[1].legend()
+plt.show()
+fig, ax;
